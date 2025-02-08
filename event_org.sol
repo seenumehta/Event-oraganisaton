05d15eb67718031b39becce75763522d5bf6a5cf
Anoop <77597998+seenumehta@users.noreply.github.com>
2025-02-08 15:58:25 +0530

    Update event_org.sol

--- event_org.sol
+++ event_org.sol
@@ -8,6 +8,7 @@ contract eventContract {
     uint price;
     uint ticketCount;
     uint ticketRemain;
+    bool isCancelled;
    }
    mapping(uint=>Event) public events;
    mapping(address=>mapping (uint=>uint)) public tickets;
@@ -17,7 +18,7 @@ contract eventContract {
    function createEvent(string calldata name,uint date,uint price,uint ticketCount)  external  {
     require(date>block.timestamp,"YOU CAN ORGANIZE FOR FUTURE");
     require(ticketCount>100 ,"you can organise if you can create more than 100 tickets");
-    events[nextid] = Event(msg.sender,name,date,price,ticketCount,ticketCount);
+    events[nextid] = Event(msg.sender,name,date,price,ticketCount,ticketCount , false);
     nextid++ ;
    }
    function buyticket(uint id ,uint quantity) external payable  {
@@ -48,5 +49,37 @@ contract eventContract {
 
 
    }
+
+   // Event Cancellation
+function cancelEvent(uint id) external {
+    Event storage eventDetails = events[id];
+
+    require(eventDetails.date != 0, "Event does not exist");
+    require(msg.sender == eventDetails.organizer, "Only organizer can cancel");
+    require(!eventDetails.isCancelled, "Event is already cancelled");
+
+    eventDetails.isCancelled = true;
+    emit EventCancelled(id);
+}
+
+// Refund System
+function claimRefund(uint id) external {
+    Event storage eventDetails = events[id];
+
+    require(eventDetails.date != 0, "Event does not exist");
+    require(eventDetails.isCancelled, "Event is not cancelled");
+    require(tickets[msg.sender][id] > 0, "No tickets to refund");
+
+    uint refundAmount = tickets[msg.sender][id] * eventDetails.price;
+    tickets[msg.sender][id] = 0;
+
+    payable(msg.sender).transfer(refundAmount);
+    emit RefundClaimed(msg.sender, id, refundAmount);
+}
+
+// Events for Logging
+event EventCancelled(uint eventId);
+event RefundClaimed(address user, uint eventId, uint amount);
+
 
 }
